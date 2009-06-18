4740f5697cf8854b5b492eef2a92f616b00b151c
retr0h <john@dewey.ws>
2009-06-18 00:08:48 -0700

    creating default dir for scripts

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -2,3 +2,10 @@ include_recipe "gems"
 include_recipe "packages"
 include_recipe "packages::iptables"
 include_recipe "packages::sshd"
+
+%w(bin sbin etc).eacho do |dir|
+  directory "/opt/bitsugar/#{dir}" do
+    recursive true
+    mode 755
+  end
+end
