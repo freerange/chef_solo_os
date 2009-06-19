fd2e68c8929ca1302ca050ee126a7c4103a33fd9
retr0h <john@dewey.ws>
2009-06-19 01:46:40 -0700

    updated readme and bootstrap procedure.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,11 +1,2 @@
 include_recipe "gems"
 include_recipe "packages"
-include_recipe "packages::iptables"
-include_recipe "packages::sshd"
-
-%w(bin sbin etc).each do |dir|
-  directory "/opt/bitsugar/#{dir}" do
-    recursive true
-    mode 0755
-  end
-end
--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -1,3 +1,6 @@
 node[:packages].each do |p|
   package p
 end
+
+include_recipe "packages::iptables"
+include_recipe "packages::sshd"
