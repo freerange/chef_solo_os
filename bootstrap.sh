75bb2225fddf80c781703464537db8808fc56db0
retr0h <john@dewey.ws>
2009-06-19 00:50:26 -0700

    adjusted bootstrap to better handle new ec2 image

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
 if [ -f "/etc/lsb-release" ]; then
-  ### remove gem dirs.
-  rm -rf /root/.gem && mkdir /root/.gem && chown 000 /root/.gem
-
   ### bootstrap with ruby.
-  apt-get update && apt-get upgrade
   apt-get install ruby ruby1.8-dev rubygems libopenssl-ruby1.8 libsqlite3-ruby irb build-essential
-  gem install rubygems-update && /var/lib/gems/1.8/bin/update_rubygems
-  rm /usr/bin/gem && ln -s /usr/bin/gem1.8 /usr/bin/gem
 
-  ### bootstrap with chef.
+  ### bootstrap rubygems.
+  gem sources -a http://gems.rubyforge.org -a http://gems.github.com && \
+  gem install rubygems-update && /var/lib/gems/1.8/bin/update_rubygems && \
+  rm /usr/bin/gem && ln -s /usr/bin/gem1.8 /usr/bin/gem && \
   gem install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org
 
   ### Run chef solo.
