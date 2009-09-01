b8a37876b6ab1f1da8efbd7f66f5c60e78526deb
retr0h <john@dewey.ws>
2009-08-31 20:15:21 -0700

    Disable of webservers by default.

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -5,3 +5,12 @@ end
 %w(packages::iptables packages::sshd ntp apache2 nginx).each do |recipe|
   include_recipe recipe
 end
+
+##
+# Default disabling of webservers.
+%w(nginx apache2).each do |webserver|
+  service webserver do
+    supports :status => true, :restart => true, :reload => true
+    action [:disable, :stop]
+  end
+end
