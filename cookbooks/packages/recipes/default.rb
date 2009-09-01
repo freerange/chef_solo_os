361f71793ccf2374be9d6ee0648800cd177d3fc6
retr0h <john@dewey.ws>
2009-08-31 19:55:25 -0700

    Added nginx and apache2

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -2,6 +2,6 @@ node[:packages].each do |p|
   package p
 end
 
-%w(packages::iptables packages::sshd ntp).each do |recipe|
+%w(packages::iptables packages::sshd ntp mysql::client mysql::server apache2 nginx).each do |recipe|
   include_recipe recipe
 end
