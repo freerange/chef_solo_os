e9f266ede47c477231dbeac65fa1a3b38022e88d
retr0h <john@dewey.ws>
2009-09-10 20:34:28 -0700

    Using github setup.

--- cookbooks/scripts/recipes/default.rb
+++ cookbooks/scripts/recipes/default.rb
@@ -1,11 +1,17 @@
-directory "/usr/local/bin" do
+script_home = "/usr/local/bin"
+
+directory script_home do
   recursive true
   mode 0755
 end
 
-%w(ghkey hostname).each do |file|
-  remote_file "/usr/local/bin/#{file}.rb" do
+%w(hostname).each do |file|
+  remote_file File.join(script_home, "#{file}.rb") do
     source "#{file}.rb"
     mode 0755
   end
+end
+
+Dir.chdir(script_home) do
+  system("git clone git://github.com/help/setup.git")
 end
