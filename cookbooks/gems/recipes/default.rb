5c975432dc59fa08694913fcb833d80a9b350833
retr0h <john@dewey.ws>
2009-09-29 23:50:23 -0700

    Install rubygems.

--- cookbooks/gems/recipes/default.rb
+++ cookbooks/gems/recipes/default.rb
@@ -1,6 +1,18 @@
+script "install_rubygems" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    curl -O http://files.rubyforge.vm.bytemark.co.uk/rubygems/rubygems-1.3.5.tgz
+    tar zxf rubygems-1.3.5.tgz
+    cd rubygems-1.3.5
+    ruby setup.rb
+    ln -sfv /usr/bin/gem1.8 /usr/bin/gem
+  EOH
+end
+
 ##
 # Taken from http://github.com/ezmobius/chef-101
-
 node[:gems].each do |gem|
   gem_package gem[:name] do
     if gem[:version] && !gem[:version].empty?
