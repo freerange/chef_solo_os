06833d3835d540c41d175c59dcda48dd7da2a7f3
James Adam <james@lazyatom.com>
2010-01-27 21:10:45 +0000

    I'm not sure if we're supposed to be able, but chef-solo complains when re-running on an existing vps if ruby-shadow isn't present

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ if [ -f "/etc/lsb-release" ]; then
   apt-get install -y git-core
 
   ### Install Ruby.
-  apt-get install -y ruby ruby1.8-dev libopenssl-ruby1.8 rdoc irb rubygems build-essential wget ssl-cert
+  apt-get install -y ruby ruby1.8-dev libopenssl-ruby1.8 rdoc irb rubygems build-essential wget ssl-cert libshadow-ruby1.8
   ### Add Rubygems bin path to $PATH
   export PATH=$PATH:/var/lib/gems/1.8/bin
 
