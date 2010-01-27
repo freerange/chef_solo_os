c44d4c107a0c49e6c2625084cc8a0cd9aaafc1b2
James Adam <james@lazyatom.com>
2010-01-27 11:30:19 +0000

    Don't install Nginx

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -2,13 +2,13 @@ node[:packages].each do |p|
   package p
 end
 
-%w(packages::iptables packages::sshd ntp apache2 nginx).each do |recipe|
+%w(packages::iptables packages::sshd ntp apache2).each do |recipe|
   include_recipe recipe
 end
 
 ##
 # Default disabling of webservers.
-%w(nginx apache2).each do |webserver|
+%w(apache2).each do |webserver|
   service webserver do
     supports :status => true, :restart => true, :reload => true
     action [:disable, :stop]
