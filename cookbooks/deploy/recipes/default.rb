0a7f85b31b0537755c192aa5cb87e80564bc667a
James Adam <james@lazyatom.com>
2010-04-15 13:06:52 +0100

    Actually, lets not even bother with a configuration variable, and lets make all the relevant directories writable.

--- cookbooks/deploy/recipes/default.rb
+++ cookbooks/deploy/recipes/default.rb
@@ -1,4 +1,12 @@
-directory @node["deploy"]["apache_vhost_directory"] do
+directory "/etc/apache2/sites-available" do
+  mode 0775
+  group "admin"
+end
+directory "/etc/apache2/sites-enabled" do
+  mode 0775
+  group "admin"
+end
+directory "/etc/apache2/ssl" do
   mode 0775
   group "admin"
 end
